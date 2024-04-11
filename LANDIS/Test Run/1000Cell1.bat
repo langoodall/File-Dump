7ad9e3ecc48d63301fd77928450ad55dcd781b19
Louis Goodall <159187174+langoodall@users.noreply.github.com>
2024-04-11 09:00:03 -0400

    Add files via upload

---
+++ LANDIS/Test Run/1000Cell1.bat
@@ -0,0 +1,12 @@
+rem Batch File to Run a Scenario 
+
+rem The 'rem' keyword indicates that this is a remark
+
+
+call landis-ii-7 1000Cell1_scenario.txt
+
+
+rem call landis-ii scenario-single-cell.txt
+
+
+pause
